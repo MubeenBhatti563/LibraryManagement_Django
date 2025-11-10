89c25648ff490bbf51a117ec7b97818ac4014737
Muhammad Mubeen <mubeeniqbal563@gmail.com>
2025-11-10 18:48:18 +0500

    active toggle added

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,8 +1,9 @@
-const hamburger = document.querySelector(".hamburger");
-
 document.addEventListener("DOMContentLoaded", () => {
-    // Hamburger
+  const hamburger = document.querySelector(".hamburger");
+  const navbarUl = document.querySelector(".navbars");
+
   hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
+    navbarUl.classList.toggle("active");
   });
 });

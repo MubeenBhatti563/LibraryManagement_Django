7e6b30366fcc9cdbeacf368fa7a8814458ca1f70
Muhammad Mubeen <mubeeniqbal563@gmail.com>
2025-11-11 21:15:08 +0500

    Removing errors after every 5s

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger");
   const navbarUl = document.querySelector(".navbars");
+  const formErrors = document.querySelectorAll(".error-messages");
 
+  // Hamburger toggle
   hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navbarUl.classList.toggle("active");
   });
+
+  // Remove form errors after 3 seconds
+  if (formErrors.length > 0) {
+    setTimeout(() => {
+      formErrors.forEach(el => el.remove());
+    }, 5000);
+  }
 });

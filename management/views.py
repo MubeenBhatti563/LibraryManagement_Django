85b7aff473b048e1c73d2b26c970edf570a0a496
Muhammad Mubeen <mubeeniqbal563@gmail.com>
2025-11-11 21:14:18 +0500

    Login and Register views

--- management/views.py
+++ management/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 def home(request):
     return render(request, "base.html")
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = LoginForm()
+
+    return render(request, "login.html", {"form": form})
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = RegisterForm()
+
+    return render(request, "register.html", {"form": form})

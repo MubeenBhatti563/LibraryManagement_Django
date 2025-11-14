8c50516c867eb7f62329287bd82e0307f543a0e8
Muhammad Mubeen <mubeeniqbal563@gmail.com>
2025-11-14 12:44:55 +0500

    Form Ajax

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -12,7 +12,14 @@ document.addEventListener("DOMContentLoaded", () => {
   // Remove form errors after 3 seconds
   if (formErrors.length > 0) {
     setTimeout(() => {
-      formErrors.forEach(el => el.remove());
+      formErrors.forEach((el) => el.remove());
     }, 5000);
   }
+  
+  document.body.addEventListener("htmx:configRequest", (event) => {
+    const csrfToken = document.querySelector("[name=csrfmiddlewaretoken]")?.value;
+    if (csrfToken) {
+      event.detail.headers["X-CSRFToken"] = csrfToken;
+    }
+  });
 });

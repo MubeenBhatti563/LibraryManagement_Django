8269e22046c864bcaa9fdf33bdabe9f3554663ea
MubeenBhatti563 <mubeeniqbal563@gmail.com>
2026-04-02 20:28:13 +0500

    Created BorrowBookForm

--- management/forms.py
+++ management/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django import forms
+from .models import IssuedBook, Book
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(
@@ -35,3 +37,36 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class BorrowBookForm(forms.ModelForm):
+    class Meta:
+        model = IssuedBook
+        fields = ['student', 'book']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'borrow_input'})
+        
+        self.fields['book'].queryset = Book.objects.filter(total_copies__gt=0)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        student = cleaned_data.get("student")
+        book = cleaned_data.get("book")
+
+        if student:
+            active_issues = IssuedBook.objects.filter(student=student, return_status=False)
+            # Check Limit
+            if active_issues.count() >= 2:
+                raise ValidationError("Student has reached the limit of 2 books.")
+            
+            # Check Duplicate
+            if active_issues.filter(book=book).exists():
+                raise ValidationError("Student already has this specific book.")
+            # Check if copies available
+            if book.total_copies < 1:
+                raise ValidationError(f"Error: {book.title} is currently out of stock")
+      
+        return cleaned_data
